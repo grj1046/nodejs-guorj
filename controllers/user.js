1165cbe8d8ae3a6684ad11d6f41c31b1dcd64990
grj1046 <guorenjun@outlook.com>
2015-07-24 23:13:15 +0800

    提交user模块

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,8 @@ var config = require('../config');
 exports.index = function (req, res, next) {
   var userName = req.params.name;
   res.render('user/index', {
-    title: "主页"
+    title: "主页",
+    username: userName
   })
   next();
 }
