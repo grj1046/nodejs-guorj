75c1aefab6258675d1acf23a5ab1819c4dafb98a
grj1046 <guorenjun@outlook.com>
2016-09-19 16:24:18 +0800

    update create & update time of article

--- common/tools.js
+++ common/tools.js
@@ -4,27 +4,34 @@ var moment = require('moment');
 moment.locale('zh-cn');//使用中文
 
 //格式化时间
-exports.formatDate = function (date, friendly) {
-  date = moment(date);
+exports.formatDate = function (date, friendly, useFullDate) {
+    date = moment(date);
 
-  if (friendly) {
-    return date.fromNow();
-  } else {
-    return date.format('YYYY-MM-DD HH:mm:ss.SSS');
-  }
+    if (friendly) {
+        return date.fromNow();
+    } else {
+        var format;
+        //if publish in current year, don't show YYYY
+        var currentYear = new Date().getFullYear();
+        if (useFullDate || date.year() !== currentYear)
+            format = "YYYY-MM-DD HH:mm:ss.SSS (ZZ)";
+        else
+            format = "MM-DD HH:mm:ss.SSS (ZZ)";
+        return date.format(format);
+    }
 };
 
 exports.validateId = function (str) {
-  return (/^[a-zA-Z0-9\-_]+$/i).test(str);
+    return (/^[a-zA-Z0-9\-_]+$/i).test(str);
 };
 
 exports.bhash = function (str, callback) {
-  //bcrypt.hash(str, 10, callback);
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(str, salt, callback);
-});
+    //bcrypt.hash(str, 10, callback);
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(str, salt, callback);
+    });
 };
 
 exports.bcompare = function (str, hash, callback) {
-  bcrypt.compare(str, hash, callback);
+    bcrypt.compare(str, hash, callback);
 };
--- models/base_model.js
+++ models/base_model.js
@@ -6,21 +6,22 @@ var tools = require('../common/tools');
 var moment = require('moment');
 
 module.exports = function (schema) {
-  schema.methods.create_at_ago = function () {
-    //如果发布时间超过一个月，则显示具体时间。
-    var friendly = moment(this.create_at).add(1, 'M').isBefore(Date.now());
-    return tools.formatDate(this.create_at, !friendly);
-  };
+    schema.methods.create_at_ago = function () {
+        //如果发布时间超过一个月，则显示具体时间。
+        var friendly = moment(this.create_at).add(1, 'M').isBefore(Date.now());
+        return tools.formatDate(this.create_at, !friendly);
+    };
 
-  schema.methods.updated_at_ago = function () {
-    return tools.formatDate(this.create_at, true);
-  };
-  
-  schema.methods.create_time = function() {
-    return moment(this.create_at).format("YYYY-MM-DD HH:mm:ss.SSS (ZZ)")
-  };
-  
-  schema.methods.update_time = function() {
-    return moment(this.create_at).format("YYYY-MM-DD HH:mm:ss.SSS (ZZ)")
-  };
+    schema.methods.updated_at_ago = function () {
+        var friendly = moment(this.create_at).add(1, 'M').isBefore(Date.now());
+        return tools.formatDate(this.update_at, !friendly);
+    };
+
+    schema.methods.create_time = function () {
+        return moment(this.create_at).format("YYYY-MM-DD HH:mm:ss.SSS (ZZ)")
+    };
+
+    schema.methods.update_time = function () {
+        return moment(this.update_at).format("YYYY-MM-DD HH:mm:ss.SSS (ZZ)")
+    };
 };
